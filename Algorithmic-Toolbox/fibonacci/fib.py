ae1b9ecf1606d55fe03368119b9c25b36f354a8a
Chris Gomes <ugali_guy@hotmail.com>
2016-04-26 21:53:49 -0500

    Submit code for Compute a Small Fibonacci Number

---
+++ Algorithmic-Toolbox/fibonacci/fib.py
@@ -0,0 +1,9 @@
+# Uses python3
+def calc_fib(n):
+	fib = [0,1] # First two Fibonacci numbers
+	for i in range(2, n+1):
+		fib.append(fib[i-1] + fib[i-2])
+	return fib[n]
+
+n = int(input())
+print(calc_fib(n))
